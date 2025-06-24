081a97bf081e0de01f27b82efc30bbc570602282
teacher <mail@mail.com>
2025-06-24 21:40:54 +0500

    Add City class

---
+++ SimpleGIS/City.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleGIS.obj;
+
+[Serializable]
+public class City
+{
+    public string Name { get; set; } = "<unknown>";
+    public double Latitude { get; set; } = 0;   // Широта
+    public double Longitude { get; set; } = 0;  // Долгота
+    
+    public City() {}
+
+    public City(string name, double latitude, double longitude)
+    {
+        Name = name;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Latitude},{Longitude})";
+    }
+}
