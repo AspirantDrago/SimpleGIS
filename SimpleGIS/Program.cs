eafa887fe84c56d83334a1210daf206151b600c9
teacher <mail@mail.com>
2025-06-24 21:39:32 +0500

    Initial commit

---
+++ SimpleGIS/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml.Serialization;
+using SimpleGIS;
+using SimpleGIS.obj;
+
+internal class Program
+{
+    private static Dictionary<short, Region> data = new Dictionary<short, Region>();
+    private static XmlSerializer xmlSerializer = new (typeof(
+        List<Region>
+    ));
+    
+    private static void Generate()
+    {
+        Region.Create(ref data, 02, "Башкортостан");
+        Region.Create(ref data, 96, "Чечня");
+        Region.Create(ref data, 48, "Москва");
+        
+        data[02].Cities.Add(new City("Стерлитамак", 1.0, 1.0));
+        data[02].Cities.Add(new City("Уфа", 2.0, 1.0));
+        data[96].Cities.Add(new City("Грозный", 10.0, 30.0));
+    }
+
+    private static void SaveToXML(string FileName)
+    {
+        try
+        {
+            using (var fs  = new FileStream(FileName, FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, data.Values.ToList());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+        }
+    }
+
+    private static void LoadFromXML(string FileName)
+    {
+        try
+        {
+            using (var fs = new FileStream(FileName, FileMode.Open))
+            {
+                var lst = (List<Region>)xmlSerializer.Deserialize(fs);
+                if (lst != null)
+                {
+                    data.Clear();
+                    foreach (var item in lst)
+                    {
+                        data[item.Code] = item;
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e.Message);
+        }
+    }
+
+    private static void Show()
+    {
+        foreach (var region in data.Values)
+        {
+            Console.WriteLine(region);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        // Generate();
+        // SaveToXML("Russia.xml");
+        LoadFromXML("Russia.xml");
+        Show();
+    }
+}
---
+++ SimpleGIS/Region.cs
@@ -0,0 +1,33 @@
+﻿using SimpleGIS.obj;
+
+namespace SimpleGIS;
+
+[Serializable]
+public class Region
+{
+    public short Code { get; set; } = -1;
+    public string Name { get; set; } = "<unknown>";
+    public List<City> Cities { get; set; } = new List<City>();
+    
+    public Region() {}
+    public Region(short code, string name) 
+    {
+        Code = code;
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        string result = $"({Code}) {Name}";
+        foreach (var city in Cities)
+        {
+            result += $"\n\t- {city}";
+        }
+        return result;
+    }
+
+    public static void Create(ref Dictionary<short, Region> dc, short code, string name)
+    {
+        dc[code] = new Region(code, name);
+    }
+}
